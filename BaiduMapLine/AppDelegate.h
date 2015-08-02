91a8c21cb9eb8e178aad2bd29702795da61ad126
ChainLV <954777574@qq.com>
2015-08-02 19:00:18 +0800

    Initial Commit

---
+++ BaiduMapLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiduMapLine
+//
+//  Created by LV on 15/8/2.
+//  Copyright (c) 2015年 linkdow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiduMapLine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiduMapLine
+//
+//  Created by LV on 15/8/2.
+//  Copyright (c) 2015年 linkdow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
